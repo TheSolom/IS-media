0b6cdc28b2364936e8ca724fa81219ea7b905b42
TheSolom <eslam.01212@gmail.com>
2024-01-15 19:53:18 +0200

    Refactor: Made jwt parser and cookies parser in one file

--- backend/src/utils/parseUtil.js
+++ backend/src/utils/parseUtil.js
@@ -1,6 +1,9 @@
-export default (cookies) =>
+export const parseCookies = (cookies) =>
   cookies.split(';').reduce((acc, cookie) => {
     const [key, value] = cookie.trim().split('=');
     acc[key.trim()] = value;
     return acc;
   }, {});
+
+export const parseJwt = (token) =>
+  JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
