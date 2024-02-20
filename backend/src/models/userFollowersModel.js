bd449b4635bfd9629862ba2477adf6c01520f2f0
TheSolom <eslam.01212@gmail.com>
2024-02-20 08:11:00 +0200

    src: Improved parameters use

--- backend/src/models/userFollowersModel.js
+++ backend/src/models/userFollowersModel.js
@@ -2,27 +2,27 @@ import connection from '../configs/database.js';
 import BaseModel from './baseModel.js';
 
 export default class UserFollowersModel extends BaseModel {
-  constructor() {
-    super('user_followers');
-  }
+    constructor() {
+        super('user_followers');
+    }
 
-  async findFollowers(followeeId, id, limit) {
-    const query = `SELECT * FROM ${this.getTableName()} 
-                    WHERE ${this.getTableName()}.userId = ? AND ${this.getTableName()}.id > ?
-                    ORDER BY ${this.getTableName()}.followDate DESC, ${this.getTableName()}.id DESC
-                    limit ${limit}`;
+    async findFollowers(followeeId, lastId, limit) {
+        const query = `SELECT * FROM ${this.getTableName()} 
+                        WHERE ${this.getTableName()}.user_id = ? AND ${this.getTableName()}.id > ?
+                        ORDER BY ${this.getTableName()}.created_at DESC, ${this.getTableName()}.id DESC
+                        limit ?`;
 
-    const result = await connection.execute(query, [followeeId, id]); // Putting limit here made an error -BUG-
-    return result;
-  }
+        const result = await connection.execute(query, [followeeId, lastId, limit.toString()]); // https://blog.stackademic.com/nodejs-with-mysql2-parameterized-query-with-limit-and-offset-e7091bfe3c47
+        return result;
+    }
 
-  async findFollowees(followerId, id, limit) {
-    const query = `SELECT * FROM ${this.getTableName()} 
-                    WHERE ${this.getTableName()}.followerId = ? AND ${this.getTableName()}.id > ?
-                    ORDER BY ${this.getTableName()}.followDate DESC, ${this.getTableName()}.id DESC
-                    limit ${limit}`;
+    async findFollowees(followerId, lastId, limit) {
+        const query = `SELECT * FROM ${this.getTableName()} 
+                        WHERE ${this.getTableName()}.follower_id = ? AND ${this.getTableName()}.id > ?
+                        ORDER BY ${this.getTableName()}.created_at DESC, ${this.getTableName()}.id DESC
+                        limit ?`;
 
-    const result = await connection.execute(query, [followerId, id]); // Putting limit here made an error -BUG-
-    return result;
-  }
+        const result = await connection.execute(query, [followerId, lastId, limit.toString()]); // https://blog.stackademic.com/nodejs-with-mysql2-parameterized-query-with-limit-and-offset-e7091bfe3c47
+        return result;
+    }
 }
