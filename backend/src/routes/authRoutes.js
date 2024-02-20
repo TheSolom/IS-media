0fb433f2f405753193befbca652782612fae051b
TheSolom <eslam.01212@gmail.com>
2024-02-20 07:45:39 +0200

    src: Added cloudinary signature route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,18 +1,20 @@
 import express from 'express';
 
 import {
-  postLogin,
-  postSignup,
-  postLogout,
-  postForgotPassword,
-  patchResetPassword,
+    postLogin,
+    postSignup,
+    postLogout,
+    postForgotPassword,
+    patchResetPassword,
+    getUploadSignature,
 } from '../controllers/authController.js';
 import {
-  loginValidation,
-  signupValidation,
-  forgotPasswordValidation,
-  resetPasswordValidation,
+    loginValidation,
+    signupValidation,
+    forgotPasswordValidation,
+    resetPasswordValidation,
 } from '../validations/authValidation.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
@@ -25,9 +27,11 @@ router.post('/logout', postLogout);
 router.post('/forgot-password', forgotPasswordValidation, postForgotPassword);
 
 router.patch(
-  '/reset-password/:token',
-  resetPasswordValidation,
-  patchResetPassword
+    '/reset-password/:token',
+    resetPasswordValidation,
+    patchResetPassword
 );
+
+router.get('/uploadSignature', authMiddleware, getUploadSignature);
 
 export default router;
