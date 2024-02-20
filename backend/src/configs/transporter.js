5571f29f0b7f5be09d284f3ed6fbb97deeccbad1
TheSolom <eslam.01212@gmail.com>
2024-02-20 07:40:13 +0200

    src: changed auth's user and pass

--- backend/src/configs/transporter.js
+++ backend/src/configs/transporter.js
@@ -1,11 +1,13 @@
 import nodemailer from 'nodemailer';
 
-export default nodemailer.createTransport({
-  host: 'smtp.resend.com',
-  secure: true,
-  port: 465,
-  auth: {
-    user: 'resend',
-    pass: 're_AmiymhnR_PhLV71K8J7vbPwChuETetH33', // process.env.SMTP_PASSWORD not working
-  },
+const transporter = nodemailer.createTransport({
+    host: 'smtp.resend.com',
+    secure: true,
+    port: 465,
+    auth: {
+        user: process.env.RESEND_USERNAME,
+        pass: process.env.RESEND_PASSWORD,
+    },
 });
+
+export default transporter;
