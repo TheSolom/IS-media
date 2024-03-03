47a76d66ff6329650b369db73f9b3d6ed970e450
TheSolom <eslam.01212@gmail.com>
2024-03-03 22:03:42 +0200

    src: Refactor update and delete functions and Add validation for conditions

--- backend/src/models/baseModel.js
+++ backend/src/models/baseModel.js
@@ -12,17 +12,20 @@ export default class BaseModel {
     }
 
     async find(conditions) {
+        const conditionKeys = Object.keys(conditions);
+
+        if (conditionKeys.length === 0)
+            throw new Error('Empty conditions object');
+
         const values = [];
         const clauses = [];
         let query = `SELECT * FROM ${this.getTableName()} `;
 
-        if (conditions) {
-            Object.entries(conditions).forEach(([key, value]) => {
-                clauses.push(`${key} = ?`);
-                values.push(value);
-            });
-            query += `WHERE ${clauses.join(' AND ')}`;
-        }
+        Object.entries(conditions).forEach(([key, value]) => {
+            clauses.push(`${key} = ?`);
+            values.push(value);
+        });
+        query += `WHERE ${clauses.join(' AND ')}`;
 
         const result = await connection.execute(query, values);
         return result;
@@ -38,30 +41,34 @@ export default class BaseModel {
     }
 
     async update(data, conditions) {
-        const setClause = Object.entries(data)
-            .map(([key]) => `${key} = ?`)
-            .join(', ');
+        const conditionKeys = Object.keys(conditions);
 
-        let whereClause = '';
-        const values = [];
-        if (conditions) {
-            whereClause = 'WHERE ';
-            Object.entries(conditions).forEach(([key, value]) => {
-                whereClause += `${key} = ? AND `;
-                values.push(value);
-            });
-            whereClause = whereClause.trim().slice(0, -4); // Remove trailing "AND"
-        }
+        if (conditionKeys.length === 0)
+            throw new Error('Empty conditions object');
 
-        const query = `UPDATE ${this.getTableName()} SET ${setClause} ${whereClause}`;
+        const setClause = Object.keys(data).map((key) => `${key} = ?`).join(', ');
+        const whereClause = conditionKeys.map(key => `${key} = ?`).join(' AND ');
 
-        const result = await connection.execute(query, [...Object.values(data), ...values]);
+        const values = [...Object.values(data), ...Object.values(conditions)];
+
+        const query = `UPDATE ${this.getTableName()} SET ${setClause} WHERE ${whereClause}`;
+
+        const result = await connection.execute(query, values);
         return result[0];
     }
 
     async delete(conditions) {
-        const query = `DELETE FROM ${this.getTableName()} ${conditions ? 'WHERE ?' : ''}`;
-        const result = await connection.execute(query, conditions);
+        const conditionKeys = Object.keys(conditions);
+
+        if (conditionKeys.length === 0)
+            throw new Error('Empty conditions object');
+
+        const whereClause = conditionKeys.map(key => `${key} = ?`).join(' AND ');
+        const values = conditionKeys.map(key => conditions[key]);
+
+        const query = `DELETE FROM ${this.getTableName()} WHERE ${whereClause}`;
+
+        const result = await connection.execute(query, values);
         return result[0];
     }
 }
