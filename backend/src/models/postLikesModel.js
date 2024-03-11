60dc406e9fcc7d131bddd16b35a60d68d68f43df
TheSolom <eslam.01212@gmail.com>
2024-03-11 05:09:52 +0200

    src: Add posts likes model

---
+++ backend/src/models/postLikesModel.js
@@ -0,0 +1,21 @@
+import connection from '../configs/database.js';
+import BaseModel from './baseModel.js';
+
+export default class PostLikesModel extends BaseModel {
+    constructor() {
+        super('post_likes');
+    }
+
+
+    async findPostLikes(postId, lastId, limit) {
+        const query = `SELECT ${this.getTableName()}.*, u.username, u.profile_picture FROM ${this.getTableName()}
+                        JOIN users AS u
+                        ON u.id = ${this.getTableName()}.user_id
+                        WHERE ${this.getTableName()}.post_id = ? AND ${this.getTableName()}.id > ?
+                        ORDER BY ${this.getTableName()}.created_at DESC, ${this.getTableName()}.id DESC
+                        limit ?`;
+
+        const result = await connection.execute(query, [postId, lastId, limit.toString()]);
+        return result;
+    }
+}
