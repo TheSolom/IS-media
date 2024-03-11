066381dfab3fbad7f5de986ecc4dfda6224281e3
TheSolom <eslam.01212@gmail.com>
2024-03-11 05:14:41 +0200

    src: Add cloudinary media delete function

---
+++ backend/src/utils/cloudinaryDelete.js
@@ -0,0 +1,14 @@
+import cloudinary from '../configs/cloudinary.js';
+
+const deleteCloudinaryMedia = async (publicId, resourceType) => {
+    if (!publicId)
+        throw new Error('No valid public id is provided');
+
+    const { result } = await cloudinary.uploader.destroy(publicId, {
+        resource_type: resourceType,
+    });
+
+    return result;
+};
+
+export default deleteCloudinaryMedia;
