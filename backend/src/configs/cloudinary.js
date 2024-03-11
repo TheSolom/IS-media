cb78257a4c0795dd21941f1e4806a1cc8db1b662
TheSolom <eslam.01212@gmail.com>
2024-03-11 05:03:09 +0200

    src: Changed return value

--- backend/src/configs/cloudinary.js
+++ backend/src/configs/cloudinary.js
@@ -1,6 +1,6 @@
-import { v2 as Cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 
-const cloudinary = Cloudinary.config({
+cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
