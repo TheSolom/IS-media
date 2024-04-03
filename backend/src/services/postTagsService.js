cc4ead61d061097636f3d1d7abd62c0aed3c929d
TheSolom <eslam.01212@gmail.com>
2024-04-03 23:18:20 +0200

    src: Add postPostTags functionality

--- backend/src/services/postTagsService.js
+++ backend/src/services/postTagsService.js
@@ -1,4 +1,9 @@
+import validator from 'validator';
+
 import PostTagsModel from '../models/postTagsModel.js';
+import TagsModel from '../models/tagsModel.js';
+import isValidUrl from '../utils/isValidUrl.js';
+import CustomError from '../utils/errorHandling.js';
 
 export const getPostTags = async (postId) => {
     const postTagsModel = new PostTagsModel();
@@ -39,6 +44,83 @@ export const getTagPosts = async (tag, lastId, limit) => {
             success: false,
             message: 'An error occurred while fetching the posts',
             status: 500,
+        };
+    }
+};
+
+export const postPostTags = async (postTitle, postContent, postId) => {
+    const tags = isValidUrl(postContent) ?
+        postTitle.match(/#([^#\s]+)/g) : postContent.match(/#([^#\s]+)/g);
+
+    if (!tags) {
+        return {
+            success: true,
+            message: 'No tags in the post',
+            status: 404,
+        };
+    }
+
+    const tagsModel = new TagsModel();
+    const postTagsModel = new PostTagsModel();
+
+    try {
+        const createTagsPromises = tags.map(async (tag) => {
+            const tagNormalized = tag.substring(1).toLowerCase();
+
+            if (!validator.isAlphanumeric(tagNormalized))
+                return Promise.reject(new CustomError(`Tag '${tag}' is not valid, should be alphanumeric`, 400));
+
+            const [tagRow] = await tagsModel.find({ tag: tagNormalized });
+
+            if (tagRow.length)
+                return postTagsModel.create({ tag_id: tagRow[0].id, post_id: postId });
+
+            const createTagResult = await tagsModel.create({ tag: tagNormalized });
+
+            return postTagsModel.create({ tag_id: createTagResult.insertId, post_id: postId });
+        });
+
+        const createPostTagsResults = await Promise.allSettled(createTagsPromises);
+
+        const rejectedPromises = [];
+        let hasCustomError = false;
+
+        createPostTagsResults.forEach(result => {
+            if (result.status === 'rejected') {
+                rejectedPromises.push(result);
+
+                if (result.reason && result.reason.status === 400)
+                    hasCustomError = true;
+            }
+        });
+
+        if (rejectedPromises.length) {
+            console.error('Some tags failed to be added:', rejectedPromises);
+            if (hasCustomError) {
+                return {
+                    success: false,
+                    message: rejectedPromises[0].reason.message,
+                    status: rejectedPromises[0].reason.status,
+                };
+            }
+
+            return {
+                success: false,
+                message: 'Failed to add some tags to the post',
+                status: 500,
+            };
+        }
+
+        return {
+            success: true,
+            message: 'Successfully added tags to the post',
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            success: false,
+            message: error.message,
+            status: error.status,
         };
     }
 };
