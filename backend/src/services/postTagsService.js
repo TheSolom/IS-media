d2167e35b589fa47b9030dccd032b38e3847df1e
TheSolom <eslam.01212@gmail.com>
2024-04-03 23:16:29 +0200

    src: Add getTagPosts functionality

--- backend/src/services/postTagsService.js
+++ backend/src/services/postTagsService.js
@@ -19,3 +19,26 @@ export const getPostTags = async (postId) => {
         };
     }
 };
+
+export const getTagPosts = async (tag, lastId, limit) => {
+    const postTagsModel = new PostTagsModel();
+
+    try {
+        const [postsRows] = await postTagsModel.findTagPosts(tag, lastId, limit);
+
+        const id = postsRows[0] ? postsRows[0].post_tag_id : 0;
+
+        return {
+            success: true,
+            lastId: id,
+            posts: postsRows
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            success: false,
+            message: 'An error occurred while fetching the posts',
+            status: 500,
+        };
+    }
+};
