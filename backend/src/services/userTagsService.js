3d080a6ca6b8980c2bab836d1e587cb3c06e9179
TheSolom <eslam.01212@gmail.com>
2024-04-14 00:21:01 +0200

    src: Add putUserTag function

--- backend/src/services/userTagsService.js
+++ backend/src/services/userTagsService.js
@@ -4,21 +4,66 @@ export const getUsedTags = async (userId, limit) => {
     const userTagsModel = new UserTagsModel();
 
     try {
-        const [usedTagsRow] = await userTagsModel.findUsedTags(userId, limit);
+        const [usedTagsRows] = await userTagsModel.findUsedTags(userId, limit);
 
-        if (!usedTagsRow.length)
+        if (!usedTagsRows.length)
             return {
                 success: false,
                 message: 'No used tags found',
                 status: 404,
             };
 
-        return { success: true, tags: usedTagsRow[0] };
+        return { success: true, tags: usedTagsRows };
     } catch (error) {
         console.error(error);
         return {
             success: false,
             message: 'An error occurred while fetching the tags',
+            status: 500,
+        };
+    }
+};
+
+export const putUserTag = async (tagsIds, userId) => {
+    if (!tagsIds) {
+        return {
+            success: false,
+            message: 'No tags provided',
+            status: 400,
+        };
+    }
+
+    const userTagsModel = new UserTagsModel();
+
+    try {
+        const createTagsPromises = tagsIds.map(async (tagId) =>
+            userTagsModel.createOrUpdateUsedTag(userId, tagId)
+        );
+
+        const createOrUpdateTagsResults = await Promise.allSettled(createTagsPromises);
+
+        const rejectedPromises = createOrUpdateTagsResults.filter(
+            (promise) => promise.status === 'rejected'
+        );
+
+        if (rejectedPromises.length) {
+            console.error('Some tags failed to be added to the user:', rejectedPromises);
+            return {
+                success: false,
+                message: 'Failed to add some tags to the user',
+                status: 500,
+            };
+        }
+
+        return {
+            success: true,
+            message: 'Successfully added tags to the user',
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            success: false,
+            message: 'An error occurred while adding user tags',
             status: 500,
         };
     }
