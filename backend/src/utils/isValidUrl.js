0cc2758b40d83e0f0ed745756c453375d81c75e6
TheSolom <eslam.01212@gmail.com>
2024-04-14 00:11:00 +0200

    src: Remove comment

--- backend/src/utils/isValidUrl.js
+++ backend/src/utils/isValidUrl.js
@@ -1,6 +1,5 @@
 const isValidUrl = (string) => {
     try {
-        // eslint-disable-next-line node/no-unsupported-features/node-builtins
         return Boolean(new URL(string));
     } catch (_) {
         return false;
