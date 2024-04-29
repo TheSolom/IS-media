6b5d8d68c891c09f533f6eec3e8de154c5592243
TheSolom <eslam.01212@gmail.com>
2024-04-29 20:27:14 +0300

    src: Change function and variable names

--- backend/src/controllers/userTagsController.js
+++ backend/src/controllers/userTagsController.js
@@ -1,24 +1,24 @@
 import CustomError from '../utils/errorHandling.js';
 import * as userTagsService from '../services/userTagsService.js';
 
-export async function getUsedTags(req, res, next) {
+export async function getMostUsedTags(req, res, next) {
     const { limit } = req.query;
 
     try {
         if (limit !== undefined && (limit === null || Number.isNaN(Number(limit)) || Number(limit) < 1))
             throw new CustomError('No valid limit is provided', 400);
 
-        const getUsedTagsResult = await userTagsService.getUsedTags(
+        const getMostUsedTagsResult = await userTagsService.getMostUsedTags(
             req.userId,
             limit ?? 5
         );
 
-        if (!getUsedTagsResult.success)
-            throw new CustomError(getUsedTagsResult.message, getUsedTagsResult.status);
+        if (!getMostUsedTagsResult.success)
+            throw new CustomError(getMostUsedTagsResult.message, getMostUsedTagsResult.status);
 
         res.status(200).json({
             success: true,
-            tags: getUsedTagsResult.tags,
+            tags: getMostUsedTagsResult.tags,
         });
     } catch (error) {
         next(error);
