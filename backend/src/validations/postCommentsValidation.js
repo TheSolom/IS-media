c40050485775bc6e866a2f8ede612fcb825e12f1
TheSolom <eslam.01212@gmail.com>
2024-05-08 17:44:50 +0300

    src: Change validation

--- backend/src/validations/postCommentsValidation.js
+++ backend/src/validations/postCommentsValidation.js
@@ -32,18 +32,15 @@ export const createPostCommentValidation = [
         .isLength({ max: 100 })
         .withMessage('Content must be at most 100 characters'),
     body('postId', 'No valid post id is provided')
-        .isNumeric()
+        .isInt({ min: 1 })
         .custom(async (value) => {
-            const postId = Number(value);
+            if (Number.isInteger(value) && value > 0) {
+                const postModel = new PostModel();
+                const [postRow] = await postModel.find({ id: value });
 
-            if (postId < 1)
-                throw new Error();
-
-            const postModel = new PostModel();
-            const [postRow] = await postModel.find({ id: postId });
-
-            if (!postRow.length)
-                throw new Error('post is not found');
+                if (!postRow.length)
+                    throw new Error('post is not found');
+            }
 
             return true;
         }),
@@ -63,10 +60,5 @@ export const updatePostCommentValidation = [
         .isLength({ max: 100 })
         .withMessage('Content must be at most 100 characters'),
     param('commentId', 'No valid comment id is provided')
-        .isNumeric()
-        .custom((value) => {
-            if (Number(value) < 1)
-                throw new Error();
-            return true;
-        }),
+        .isInt({ min: 1 })
 ];
