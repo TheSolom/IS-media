a938c24595a9cfa4ea0d96f00189a13bd61a1209
TheSolom <eslam.01212@gmail.com>
2024-05-08 14:15:55 +0300

    src: Add new functionality and Add validation for routes

--- backend/src/routes/postCommentsRoutes.js
+++ backend/src/routes/postCommentsRoutes.js
@@ -1,22 +1,29 @@
 import express from 'express';
 
 import {
+    getPostComment,
     getPostComments,
     postPostComment,
     updatePostComment,
     deletePostComment,
 } from '../controllers/postCommentsController.js';
+import {
+    createPostCommentValidation,
+    updatePostCommentValidation
+} from '../validations/postCommentsValidation.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
+router.get('/comments/:commentId', getPostComment);
+
 router.get('/:postId/comments', getPostComments);
 
-router.post('/comments', postPostComment);
+router.post('/comments', createPostCommentValidation, postPostComment);
 
-router.put('/comments/:commentId', updatePostComment);
+router.put('/comments/:commentId', updatePostCommentValidation, updatePostComment);
 
 router.delete('/comments/:commentId', deletePostComment);
 
