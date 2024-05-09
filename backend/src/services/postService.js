f592ff0b0e0d5aea33d5391b85c2c81d9be5bb06
TheSolom <eslam.01212@gmail.com>
2024-05-09 11:32:39 +0300

    src: Move function to be imported from another folder

--- backend/src/services/postService.js
+++ backend/src/services/postService.js
@@ -1,31 +1,6 @@
 import PostModel from '../models/postModel.js';
 import shuffleArray from '../utils/shuffleArray.js';
-
-const postMapper = (postRow) =>
-    postRow.map((post) => (
-        {
-            postId: post.id,
-            title: post.title,
-            content: post.content,
-            createdAt: post.created_at,
-            updatedAt: post.updated_at,
-            author: {
-                id: post.author_id,
-                username: post.username,
-                profilePicture: post.profile_picture
-            },
-            parentPost: post.parent_id ? {
-                title: post.parent_title,
-                content: post.parent_content,
-                createdAt: post.parent_created_at,
-                author: {
-                    id: post.parent_author_id,
-                    username: post.parent_username,
-                    profilePicture: post.parent_profile_picture
-                }
-            } : null
-        }
-    ));
+import postMapper from '../utils/mappers/postMapper.js';
 
 export const getUserPosts = async (userId, lastId, limit) => {
     const postModel = new PostModel();
