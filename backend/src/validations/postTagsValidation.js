8ff26754a553a4677bf10de7d5509b2816848616
TheSolom <eslam.01212@gmail.com>
2024-05-11 12:03:44 +0300

    src: Fix typo

--- backend/src/validations/postTagsValidation.js
+++ backend/src/validations/postTagsValidation.js
@@ -5,7 +5,7 @@ const getTagPostsValidation = [
         .trim()
         .exists({ values: 'falsy' })
         .withMessage('No valid tag is provided')
-        .length({ max: 499 })
+        .isLength({ max: 499 })
         .withMessage('Tag must be between 1 - 499 characters long')
         .isAlphanumeric()
         .withMessage('Tag must only contain letters and numbers'),
