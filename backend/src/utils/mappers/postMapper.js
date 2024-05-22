f321fac801b0e1ca62cd2e2914c75089fcdfbd23
TheSolom <eslam.01212@gmail.com>
2024-05-22 22:33:01 +0300

    src: Refactor function

--- backend/src/utils/mappers/postMapper.js
+++ backend/src/utils/mappers/postMapper.js
@@ -1,27 +1,25 @@
-const postMapper = (postRow) =>
-    postRow.map((post) => (
-        {
-            postId: post.id,
-            title: post.title,
-            content: post.content,
-            createdAt: post.created_at,
-            updatedAt: post.updated_at,
-            author: {
-                id: post.author_id,
-                username: post.username,
-                profilePicture: post.profile_picture
-            },
-            parentPost: post.parent_id ? {
-                title: post.parent_title,
-                content: post.parent_content,
-                createdAt: post.parent_created_at,
-                author: {
-                    id: post.parent_author_id,
-                    username: post.parent_username,
-                    profilePicture: post.parent_profile_picture
-                }
-            } : null
+const postMapper = (postRow) => ({
+    id: postRow.id,
+    title: postRow.title,
+    content: postRow.content,
+    createdAt: postRow.created_at,
+    updatedAt: postRow.updated_at,
+    author: {
+        id: postRow.author_id,
+        username: postRow.username,
+        profilePicture: postRow.profile_picture
+    },
+    parentPost: postRow.parent_id ? {
+        id: postRow.parent_id,
+        title: postRow.parent_title,
+        content: postRow.parent_content,
+        createdAt: postRow.parent_created_at,
+        author: {
+            id: postRow.parent_author_id,
+            username: postRow.parent_username,
+            profilePicture: postRow.parent_profile_picture
         }
-    ));
+    } : null
+});
 
 export default postMapper;
