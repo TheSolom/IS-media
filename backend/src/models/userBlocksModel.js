c2dc61746e2336291b1f422a7acbfa2398caba1a
Islam Ashraf <eslam.01212@gmail.com>
2024-06-09 15:58:35 +0300

    src: Move function above

--- backend/src/models/userBlocksModel.js
+++ backend/src/models/userBlocksModel.js
@@ -6,6 +6,15 @@ export default class ConversationModel extends BaseModel {
         super('user_blocklist');
     }
 
+    async findUserBlockStatus(userId1, userId2) {
+        const query = `SELECT * FROM ${this.getTableName()}
+                            WHERE (${this.getTableName()}.user_id = ? AND ${this.getTableName()}.blocked_id = ?)
+                            OR (${this.getTableName()}.user_id = ? AND ${this.getTableName()}.blocked_id = ?)`;
+
+        const result = await connection.execute(query, [userId1, userId2, userId2, userId1]);
+        return result;
+    }
+
     async findBlocks(blockerId, lastId, limit) {
         const query = `SELECT ${this.getTableName()}.*, users.username, users.profile_picture FROM ${this.getTableName()}
                         JOIN users
@@ -15,15 +24,6 @@ export default class ConversationModel extends BaseModel {
                         limit ${limit}`;
 
         const result = await connection.execute(query, [blockerId, lastId.toString()]);
-        return result;
-    }
-
-    async findUserBlockStatus(userId1, userId2) {
-        const query = `SELECT * FROM ${this.getTableName()}
-                        WHERE (${this.getTableName()}.user_id = ? AND ${this.getTableName()}.blocked_id = ?)
-                        OR (${this.getTableName()}.user_id = ? AND ${this.getTableName()}.blocked_id = ?)`;
-
-        const result = await connection.execute(query, [userId1, userId2, userId2, userId1]);
         return result;
     }
 }
